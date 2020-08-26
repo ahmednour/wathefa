15dbc54c8d4e996e84107b57caa12a55d60f8771
Ahmed Nour <darklevel152@gmail.com>
2020-08-26 23:06:51 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -41,6 +41,70 @@ AOS.init({
     // Scrollax
     $.Scrollax();
 
+    var carousel = function() {
+        $('.carousel-testimony').owlCarousel({
+            center: true,
+            loop: true,
+            items: 1,
+            margin: 30,
+            stagePadding: 0,
+            nav: false,
+            navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 5
+                }
+            }
+        });
+
+    };
+    carousel();
+
+    // scroll
+    var scrollWindow = function() {
+        $(window).scroll(function() {
+            var $w = $(this),
+                st = $w.scrollTop(),
+                navbar = $('.top-nav'),
+                sd = $('.js-scroll-wrap');
+
+            if (st > 150) {
+                if (!navbar.hasClass('scrolled')) {
+                    navbar.addClass('scrolled');
+                }
+            }
+            if (st < 150) {
+                if (navbar.hasClass('scrolled')) {
+                    navbar.removeClass('scrolled sleep');
+                }
+            }
+            if (st > 350) {
+                if (!navbar.hasClass('awake')) {
+                    navbar.addClass('awake');
+                }
+
+                if (sd.length > 0) {
+                    sd.addClass('sleep');
+                }
+            }
+            if (st < 350) {
+                if (navbar.hasClass('awake')) {
+                    navbar.removeClass('awake');
+                    navbar.addClass('sleep');
+                }
+                if (sd.length > 0) {
+                    sd.removeClass('sleep');
+                }
+            }
+        });
+    };
+    scrollWindow();
 
     var contentWayPoint = function() {
         var i = 0;
